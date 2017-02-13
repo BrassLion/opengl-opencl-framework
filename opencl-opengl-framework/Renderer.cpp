24e29ae8e9d059cba3c0d1ac03fdc2a2a3751853
Sam Hall <BrassLion@users.noreply.github.com>
2017-02-13 02:53:43 +0000

    Darkened clear colour.

--- opencl-opengl-framework/Renderer.cpp
+++ opencl-opengl-framework/Renderer.cpp
@@ -15,7 +15,7 @@ void Renderer::draw(std::shared_ptr<Object> root_node)
 {
     // Render
     // Clear the colorbuffer
-    glClearColor(0.1f, 0.2f, 0.2f, 1.0f);
+    glClearColor(0.05f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
     glEnable(GL_DEPTH_TEST);
